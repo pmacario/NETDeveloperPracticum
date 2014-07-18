7577e04a2169e2d27860f2afc0d6c15f6c31eedb
pmacario <sta12quest@gmail.com>
2014-07-18 09:20:55 -0500

    Cleaned up batch file

--- BuildAndTest.bat
+++ BuildAndTest.bat
@@ -4,26 +4,22 @@ set X86MachineMSBuildPath="C:\Program Files\MSBuild\12.0\bin\MSBuild.exe"
 set X64Machine32BitMSBuildPath="C:\Program Files (x86)\MSBuild\12.0\bin\MSBuild.exe"
 set X64Machine64BitMSBuildPath="C:\Program Files (x86)\MSBuild\12.0\bin\amd64\MSBuild.exe"
 
+rem Find the MSBuild.exe file path.
 if exist %X86MachineMSBuildPath% (
- 	rem echo Found in X86MachineMSBuildPath
 	set MSBuildPath=%X86MachineMSBuildPath%
 ) else if exist %X64Machine32BitMSBuildPath% (
-	rem echo Found in X64Machine32BitMSBuildPath
 	set MSBuildPath=%X64Machine32BitMSBuildPath%
 ) else if exist %X64Machine64BitMSBuildPath% (
-	rem echo Found in X64Machine64BitMSBuildPath
 	set MSBuildPath=%X64Machine64BitMSBuildPath%
 ) else (
 	echo MSBuild.exe path not found
 )
 
-rem echo %MSBuildPath%
-
 if defined MSBuildPath (
-	rem echo MSBuildPath defined
+	rem If the MSBuild.exe file path has been located, use it to 
+	rem clean and build the solution.
 	call %MSBuildPath% NETDeveloperPracticum.sln /p:Configuration=Release /t:Clean;Build
 	cls
+	rem Launch the console application to allow for testing.
 	start "Order Food" OrderFood\bin\Release\OrderFood.exe
-rem ) else (
-	rem echo MSBuildPath not defined
 )
